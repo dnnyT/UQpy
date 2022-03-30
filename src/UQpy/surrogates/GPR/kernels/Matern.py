587964ca67f55a781964a1a29b0799311c57ad9f
Mohit Singh Chauhan <mchauha1@jhu.edu>
2022-03-30 13:56:37 -0400

    GPR: Pull changes from v4 branch

--- src/UQpy/surrogates/GPR/kernels/Matern.py
+++ src/UQpy/surrogates/GPR/kernels/Matern.py
@@ -1,5 +1,6 @@
 from UQpy.surrogates.gpr.kernels.baseclass.Kernel import *
 from scipy.spatial.distance import pdist, cdist, squareform
+from scipy.special import gamma, kv
 
 
 class Matern(Kernel):
@@ -7,8 +8,19 @@ class Matern(Kernel):
         self.nu = nu
 
     def c(self, x, s, params):
-        stack = Kernel.check_samples_and_return_stack(x, s)
-        stack1 = cdist(x, s, metric='euclidean')
-        print(stack, stack1)
-        k = params[-1]**2 * np.exp(np.sum(-(1/(2*params[:-1]**2)) * (stack ** 2), axis=2))
+        stack = cdist(x/params, s/params, metric='euclidean')
+        if self.nu == 0.5:
+            k = np.exp(-stack)
+        elif self.nu == 1.5:
+            k = (1+np.sqrt(3)*stack)*np.exp(-np.sqrt(3)*stack)
+        elif self.nu == 2.5:
+            k = (1+np.sqrt(5)*stack+5*(stack**2)/3)*np.exp(-np.sqrt(5)*stack)
+        elif self.nu == np.inf:
+            k = np.exp(-(stack**2)/2)
+        else:
+            stack *= np.sqrt(2*self.nu)
+            tmp = 1/(gamma(self.nu)*(2**(self.nu-1)))
+            tmp1 = stack ** self.nu
+            tmp2 = kv(tmp1)
+            k = tmp*tmp1*tmp2
         return k
