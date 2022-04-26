8ac8c2a24df9c392926cb79ded076fd0f6a94a0a
Dimitris Tsapetis <dimtsap@hotmail.com>
2022-04-26 12:28:51 -0400

    Updates adaptive kriging tests

--- tests/unit_tests/sampling/test_adaptive_kriging.py
+++ tests/unit_tests/sampling/test_adaptive_kriging.py
@@ -3,7 +3,7 @@ import pytest
 from UQpy.run_model.model_execution.PythonModel import PythonModel
 from UQpy.utilities.MinimizeOptimizer import MinimizeOptimizer
 
-from UQpy.surrogates.kriging.Kriging import Kriging
+from UQpy.surrogates.gaussian_process.GaussianProcessRegression import GaussianProcessRegression
 from UQpy.sampling import MonteCarloSampling, AdaptiveKriging
 from UQpy.run_model.RunModel import RunModel
 from UQpy.distributions.collection import Normal
@@ -12,168 +12,165 @@ import shutil
 
 
 def test_akmcs_weighted_u():
-    from UQpy.surrogates.kriging.regression_models.LinearRegression import LinearRegression
-    from UQpy.surrogates.kriging.correlation_models.ExponentialCorrelation import ExponentialCorrelation
+    from UQpy.surrogates.gaussian_process.regression_models.LinearRegression import LinearRegression
+    from UQpy.surrogates.gaussian_process.kernels.RBF import RBF
 
     marginals = [Normal(loc=0., scale=4.), Normal(loc=0., scale=4.)]
     x = MonteCarloSampling(distributions=marginals, nsamples=20, random_state=0)
     model = PythonModel(model_script='series.py', model_object_name="series")
     rmodel = RunModel(model=model)
     regression_model = LinearRegression()
-    correlation_model = ExponentialCorrelation()
-    K = Kriging(regression_model=regression_model, correlation_model=correlation_model,
-                optimizer=MinimizeOptimizer('l-bfgs-b'),
-                optimizations_number=10, correlation_model_parameters=[1, 1], random_state=1)
+    correlation_model = RBF()
+    K = GaussianProcessRegression(kernel=correlation_model, hyperparameters=[1, 1, 0.1], random_state=1,
+                                  optimizer=MinimizeOptimizer('l-bfgs-b'), regression_model=regression_model,
+                                  optimizations_number=10, bounds=[[1e-2, 20], [1e-2, 20], [1e-5, 1e-2]],)
     # OPTIONS: 'U', 'EFF', 'Weighted-U'
     learning_function = WeightedUFunction(weighted_u_stop=2)
     a = AdaptiveKriging(distributions=marginals, runmodel_object=rmodel, surrogate=K,
-                        learning_nsamples=10**3, n_add=1, learning_function=learning_function,
+                        learning_nsamples=10 ** 3, n_add=1, learning_function=learning_function,
                         random_state=2)
     a.run(nsamples=25, samples=x.samples)
 
-    assert a.samples[23, 0] == 1.083176685073489
+    assert a.samples[23, 0] == -2.6302040474312056
     assert a.samples[20, 1] == 0.20293978126855253
 
 
-
 def test_akmcs_u():
-    from UQpy.surrogates.kriging.regression_models.LinearRegression import LinearRegression
-    from UQpy.surrogates.kriging.correlation_models.ExponentialCorrelation import ExponentialCorrelation
+    from UQpy.surrogates.gaussian_process.regression_models.LinearRegression import LinearRegression
+    from UQpy.surrogates.gaussian_process.kernels.RBF import RBF
 
     marginals = [Normal(loc=0., scale=4.), Normal(loc=0., scale=4.)]
     x = MonteCarloSampling(distributions=marginals, nsamples=20, random_state=1)
     model = PythonModel(model_script='series.py', model_object_name="series")
     rmodel = RunModel(model=model)
     regression_model = LinearRegression()
-    correlation_model = ExponentialCorrelation()
-    K = Kriging(regression_model=regression_model, correlation_model=correlation_model,
-                optimizer=MinimizeOptimizer('l-bfgs-b'),
-                optimizations_number=10, correlation_model_parameters=[1, 1], random_state=0)
+    correlation_model = RBF()
+    K = GaussianProcessRegression(kernel=correlation_model, hyperparameters=[1, 1, 0.1], random_state=1,
+                                  optimizer=MinimizeOptimizer('l-bfgs-b'), regression_model=regression_model,
+                                  optimizations_number=10, bounds=[[1e-2, 20], [1e-2, 20], [1e-5, 1e-2]],)
     # OPTIONS: 'U', 'EFF', 'Weighted-U'
     learning_function = UFunction(u_stop=2)
     a = AdaptiveKriging(distributions=marginals, runmodel_object=rmodel, surrogate=K,
-                        learning_nsamples=10**3, n_add=1, learning_function=learning_function,
+                        learning_nsamples=10 ** 3, n_add=1, learning_function=learning_function,
                         random_state=2)
     a.run(nsamples=25, samples=x.samples)
 
-    assert a.samples[23, 0] == -4.141979058326188
-    assert a.samples[20, 1] == -1.6476534435429009
-
+    assert a.samples[23, 0] == 2.3364698907681873
+    assert a.samples[20, 1] == -0.35419762304600505
 
 
 def test_akmcs_expected_feasibility():
-    from UQpy.surrogates.kriging.regression_models.LinearRegression import LinearRegression
-    from UQpy.surrogates.kriging.correlation_models.ExponentialCorrelation import ExponentialCorrelation
+    from UQpy.surrogates.gaussian_process.regression_models.LinearRegression import LinearRegression
+    from UQpy.surrogates.gaussian_process.kernels.RBF import RBF
 
     marginals = [Normal(loc=0., scale=4.), Normal(loc=0., scale=4.)]
     x = MonteCarloSampling(distributions=marginals, nsamples=20, random_state=1)
     model = PythonModel(model_script='series.py', model_object_name="series")
     rmodel = RunModel(model=model)
     regression_model = LinearRegression()
-    correlation_model = ExponentialCorrelation()
-    K = Kriging(regression_model=regression_model, correlation_model=correlation_model,
-                optimizations_number=10, correlation_model_parameters=[1, 1],
-                optimizer=MinimizeOptimizer('l-bfgs-b'),)
+    correlation_model = RBF()
+    K = GaussianProcessRegression(kernel=correlation_model, hyperparameters=[1, 1, 0.1], random_state=1,
+                                  bounds=[[1e-2, 20], [1e-2, 20], [1e-5, 1e-2]],
+                                  optimizer=MinimizeOptimizer('l-bfgs-b'), regression_model=regression_model,
+                                  optimizations_number=10)
     # OPTIONS: 'U', 'EFF', 'Weighted-U'
     learning_function = ExpectedFeasibility(eff_a=0, eff_epsilon=2, eff_stop=0.001)
     a = AdaptiveKriging(distributions=marginals, runmodel_object=rmodel, surrogate=K,
-                        learning_nsamples=10**3, n_add=1, learning_function=learning_function,
+                        learning_nsamples=10 ** 3, n_add=1, learning_function=learning_function,
                         random_state=2)
     a.run(nsamples=25, samples=x.samples)
 
-    assert a.samples[23, 0] == 1.366058523912817
-    assert a.samples[20, 1] == -12.914668932772358
-
+    assert a.samples[23, 0] == -0.2710594625659133
+    assert a.samples[20, 1] == 0.8104109752887586
 
 
 def test_akmcs_expected_improvement():
-    from UQpy.surrogates.kriging.regression_models.LinearRegression import LinearRegression
-    from UQpy.surrogates.kriging.correlation_models.ExponentialCorrelation import ExponentialCorrelation
+    from UQpy.surrogates.gaussian_process.regression_models.LinearRegression import LinearRegression
+    from UQpy.surrogates.gaussian_process.kernels.RBF import RBF
 
     marginals = [Normal(loc=0., scale=4.), Normal(loc=0., scale=4.)]
     x = MonteCarloSampling(distributions=marginals, nsamples=20, random_state=1)
     model = PythonModel(model_script='series.py', model_object_name="series")
     rmodel = RunModel(model=model)
     regression_model = LinearRegression()
-    correlation_model = ExponentialCorrelation()
-    K = Kriging(regression_model=regression_model, correlation_model=correlation_model,
-                optimizations_number=10, correlation_model_parameters=[1, 1],
-                optimizer=MinimizeOptimizer('l-bfgs-b'),)
+    correlation_model = RBF()
+    K = GaussianProcessRegression(kernel=correlation_model, hyperparameters=[1, 1, 0.1], random_state=1,
+                                  bounds=[[1e-2, 20], [1e-2, 20], [1e-5, 1e-2]],
+                                  optimizer=MinimizeOptimizer('l-bfgs-b'), regression_model=regression_model,
+                                  optimizations_number=10)
     # OPTIONS: 'U', 'EFF', 'Weighted-U'
     learning_function = ExpectedImprovement()
     a = AdaptiveKriging(distributions=marginals, runmodel_object=rmodel, surrogate=K,
-                        learning_nsamples=10**3, n_add=1, learning_function=learning_function,
+                        learning_nsamples=10 ** 3, n_add=1, learning_function=learning_function,
                         random_state=2)
     a.run(nsamples=25, samples=x.samples)
 
-    assert a.samples[23, 0] == 4.553078100499578
-    assert a.samples[20, 1] == -3.508949564718469
-
+    assert a.samples[20, 1] == 2.1884195849577157
 
 
 def test_akmcs_expected_improvement_global_fit():
-    from UQpy.surrogates.kriging.regression_models.LinearRegression import LinearRegression
-    from UQpy.surrogates.kriging.correlation_models.ExponentialCorrelation import ExponentialCorrelation
+    from UQpy.surrogates.gaussian_process.regression_models.LinearRegression import LinearRegression
+    from UQpy.surrogates.gaussian_process.kernels.RBF import RBF
 
     marginals = [Normal(loc=0., scale=4.), Normal(loc=0., scale=4.)]
     x = MonteCarloSampling(distributions=marginals, nsamples=20, random_state=1)
     model = PythonModel(model_script='series.py', model_object_name="series")
     rmodel = RunModel(model=model)
     regression_model = LinearRegression()
-    correlation_model = ExponentialCorrelation()
-    K = Kriging(regression_model=regression_model, correlation_model=correlation_model,
-                optimizations_number=10, correlation_model_parameters=[1, 1],
-                optimizer=MinimizeOptimizer('l-bfgs-b'),)
+    correlation_model = RBF()
+    K = GaussianProcessRegression(kernel=correlation_model, hyperparameters=[1, 1, 0.1], random_state=1,
+                                  optimizer=MinimizeOptimizer('l-bfgs-b'), regression_model=regression_model,
+                                  optimizations_number=10, bounds=[[1e-2, 20], [1e-2, 20], [1e-5, 1e-2]],)
     # OPTIONS: 'U', 'EFF', 'Weighted-U'
     learning_function = ExpectedImprovementGlobalFit()
     a = AdaptiveKriging(distributions=marginals, runmodel_object=rmodel, surrogate=K,
-                        learning_nsamples=10**3, n_add=1, learning_function=learning_function,
+                        learning_nsamples=10 ** 3, n_add=1, learning_function=learning_function,
                         random_state=2)
     a.run(nsamples=25, samples=x.samples)
 
-    assert a.samples[23, 0] == 11.939859785098493
-    assert a.samples[20, 1] == -8.429899469300118
+    assert a.samples[23, 0] == -10.24267076486663
+    assert a.samples[20, 1] == 0.17610325620498946
 
 
 def test_akmcs_samples_error():
-    from UQpy.surrogates.kriging.regression_models.LinearRegression import LinearRegression
-    from UQpy.surrogates.kriging.correlation_models.ExponentialCorrelation import ExponentialCorrelation
+    from UQpy.surrogates.gaussian_process.regression_models.LinearRegression import LinearRegression
+    from UQpy.surrogates.gaussian_process.kernels.RBF import RBF
 
     marginals = [Normal(loc=0., scale=4.), Normal(loc=0., scale=4.)]
     x = MonteCarloSampling(distributions=marginals, nsamples=20, random_state=0)
     model = PythonModel(model_script='series.py', model_object_name="series")
     rmodel = RunModel(model=model)
     regression_model = LinearRegression()
-    correlation_model = ExponentialCorrelation()
-    K = Kriging(regression_model=regression_model, correlation_model=correlation_model,
-                optimizer=MinimizeOptimizer('l-bfgs-b'),
-                optimizations_number=10, correlation_model_parameters=[1, 1], random_state=1)
+    correlation_model = RBF()
+    K = GaussianProcessRegression(kernel=correlation_model, hyperparameters=[1, 1, 0.1], random_state=1,
+                                  optimizer=MinimizeOptimizer('l-bfgs-b'), regression_model=regression_model,
+                                  optimizations_number=10, bounds=[[1e-2, 20], [1e-2, 20], [1e-5, 1e-2]],)
     # OPTIONS: 'U', 'EFF', 'Weighted-U'
     learning_function = WeightedUFunction(weighted_u_stop=2)
     with pytest.raises(NotImplementedError):
-        a = AdaptiveKriging(distributions=[Normal(loc=0., scale=4.)]*3, runmodel_object=rmodel, surrogate=K,
-                            learning_nsamples=10**3, n_add=1, learning_function=learning_function,
+        a = AdaptiveKriging(distributions=[Normal(loc=0., scale=4.)] * 3, runmodel_object=rmodel, surrogate=K,
+                            learning_nsamples=10 ** 3, n_add=1, learning_function=learning_function,
                             random_state=2, samples=x.samples)
 
 
 def test_akmcs_u_run_from_init():
-    from UQpy.surrogates.kriging.regression_models.LinearRegression import LinearRegression
-    from UQpy.surrogates.kriging.correlation_models.ExponentialCorrelation import ExponentialCorrelation
+    from UQpy.surrogates.gaussian_process.regression_models.LinearRegression import LinearRegression
+    from UQpy.surrogates.gaussian_process.kernels.RBF import RBF
 
     marginals = [Normal(loc=0., scale=4.), Normal(loc=0., scale=4.)]
     x = MonteCarloSampling(distributions=marginals, nsamples=20, random_state=1)
     model = PythonModel(model_script='series.py', model_object_name="series")
     rmodel = RunModel(model=model)
     regression_model = LinearRegression()
-    correlation_model = ExponentialCorrelation()
-    K = Kriging(regression_model=regression_model, correlation_model=correlation_model,
-                optimizer=MinimizeOptimizer('l-bfgs-b'),
-                optimizations_number=10, correlation_model_parameters=[1, 1], random_state=0)
+    correlation_model = RBF()
+    K = GaussianProcessRegression(kernel=correlation_model, hyperparameters=[1, 1, 0.1], random_state=1,
+                                  optimizer=MinimizeOptimizer('l-bfgs-b'), regression_model=regression_model,
+                                  optimizations_number=10, bounds=[[1e-2, 20], [1e-2, 20], [1e-5, 1e-2]])
     # OPTIONS: 'U', 'EFF', 'Weighted-U'
     learning_function = UFunction(u_stop=2)
     a = AdaptiveKriging(distributions=marginals, runmodel_object=rmodel, surrogate=K,
-                        learning_nsamples=10**3, n_add=1, learning_function=learning_function,
+                        learning_nsamples=10 ** 3, n_add=1, learning_function=learning_function,
                         random_state=2, nsamples=25, samples=x.samples)
 
-    assert a.samples[23, 0] == -4.141979058326188
-    assert a.samples[20, 1] == -1.6476534435429009
+    assert a.samples[23, 0] == 2.3364698907681873
+    assert a.samples[20, 1] == -0.35419762304600505
--- tests/unit_tests/sampling/test_refined_stratified.py
+++ tests/unit_tests/sampling/test_refined_stratified.py
@@ -9,7 +9,7 @@ from UQpy.sampling.stratified_sampling.RefinedStratifiedSampling import *
 from UQpy.sampling.stratified_sampling.refinement.RandomRefinement import *
 from UQpy.sampling.stratified_sampling.strata.VoronoiStrata import *
 from UQpy.run_model.RunModel import *
-from UQpy.surrogates.kriging.Kriging import Kriging
+from UQpy.surrogates.gaussian_process.GaussianProcessRegression import GaussianProcessRegression
 
 
 def test_rss_simple_rectangular():
@@ -72,11 +72,12 @@ def test_rect_gerss():
     x = TrueStratifiedSampling(distributions=marginals, strata_object=strata, nsamples_per_stratum=1)
     model = PythonModel(model_script='python_model_function.py', model_object_name="y_func")
     rmodel = RunModel(model=model)
-    from UQpy.surrogates.kriging.regression_models import LinearRegression
-    from UQpy.surrogates.kriging.correlation_models import ExponentialCorrelation
+    from UQpy.surrogates.gaussian_process.regression_models import LinearRegression
+    from UQpy.surrogates.gaussian_process.kernels.RBF import RBF
 
-    K = Kriging(regression_model=LinearRegression(), correlation_model=ExponentialCorrelation(), optimizations_number=20, random_state=0,
-                correlation_model_parameters=[1, 1], optimizer=MinimizeOptimizer('l-bfgs-b'), )
+    K = GaussianProcessRegression(kernel=RBF(), hyperparameters=[1, 1, 0.1], random_state=0,
+                                  optimizer=MinimizeOptimizer('l-bfgs-b'), regression_model=LinearRegression(),
+                                  optimizations_number=20, bounds=[[1e-2, 20], [1e-2, 20], [1e-5, 1e-2]],)
     K.fit(samples=x.samples, values=rmodel.qoi_list)
     refinement = GradientEnhancedRefinement(strata=x.strata_object, runmodel_object=rmodel,
                                             surrogate=K, nearest_points_number=4)
@@ -85,18 +86,6 @@ def test_rect_gerss():
     assert np.allclose(z.samples, np.array([[0.417022, 0.36016225], [1.00011437, 0.15116629],
                                             [0.14675589, 0.5461693], [1.18626021, 0.67278036],
                                             [1.51296312, 0.77483124], [0.74237455, 0.66026822]]))
-    # assert np.allclose(z.samples, np.array([[0.417022, 0.36016225], [1.00011437, 0.15116629],
-    #                                         [0.14675589, 0.5461693], [1.18626021, 0.67278036],
-    #                                         [1.59254104, 0.96577043], [1.97386531, 0.24237455]]))
-    # assert np.allclose(z.samples, np.array([[0.417022, 0.36016225], [1.00011437, 0.15116629],
-    #                                         [0.14675589, 0.5461693], [1.18626021, 0.67278036],
-    #                                         [1.59254104, 0.96577043], [1.7176612, 0.2101839]]))
-    # assert np.allclose(z.samplesU01, np.array([[0.208511, 0.36016225], [0.50005719, 0.15116629],
-    #                                            [0.07337795, 0.5461693], [0.59313011, 0.67278036],
-    #                                            [0.79627052, 0.96577043], [0.98693265, 0.24237455]]))
-    # assert np.allclose(z.samplesU01, np.array([[0.208511, 0.36016225], [0.50005719, 0.15116629],
-    #                                            [0.07337795, 0.5461693], [0.59313011, 0.67278036],
-    #                                            [0.79627052, 0.96577043], [0.8588306 , 0.2101839]]))
 
 
 def test_vor_rss():
@@ -124,13 +113,14 @@ def test_vor_gerss():
     marginals = [Uniform(loc=0., scale=2.), Uniform(loc=0., scale=1.)]
     strata_vor = VoronoiStrata(seeds_number=4, dimension=2, random_state=10)
     x_vor = TrueStratifiedSampling(distributions=marginals, strata_object=strata_vor, nsamples_per_stratum=1, )
-    from UQpy.surrogates.kriging.regression_models.LinearRegression import LinearRegression
-    from UQpy.surrogates.kriging.correlation_models.ExponentialCorrelation import ExponentialCorrelation
+    from UQpy.surrogates.gaussian_process.regression_models.LinearRegression import LinearRegression
+    from UQpy.surrogates.gaussian_process.kernels.RBF import RBF
     model = PythonModel(model_script='python_model_function.py', model_object_name="y_func")
     rmodel = RunModel(model=model)
-    K_ = Kriging(regression_model=LinearRegression(), correlation_model=ExponentialCorrelation(), optimizations_number=20,
-                 optimizer=MinimizeOptimizer('l-bfgs-b'), random_state=0,
-                 correlation_model_parameters=[1, 1])
+
+    K_ = GaussianProcessRegression(kernel=RBF(), hyperparameters=[1, 1, 0.1], random_state=0,
+                                  optimizer=MinimizeOptimizer('l-bfgs-b'), regression_model=LinearRegression(),
+                                  optimizations_number=20, bounds=[[1e-2, 20], [1e-2, 20], [1e-5, 1e-2]], )
 
     K_.fit(samples=x_vor.samples, values=rmodel.qoi_list)
     z_vor = RefinedStratifiedSampling(stratified_sampling=x_vor, nsamples=6, random_state=x_vor.random_state,
@@ -165,13 +155,15 @@ def test_rss_runmodel_object():
     marginals = [Uniform(loc=0., scale=2.), Uniform(loc=0., scale=1.)]
     strata = RectangularStrata(strata_number=[2, 2])
     x = TrueStratifiedSampling(distributions=marginals, strata_object=strata, nsamples_per_stratum=1, random_state=1)
-    from UQpy.surrogates.kriging.regression_models import LinearRegression
-    from UQpy.surrogates.kriging.correlation_models import ExponentialCorrelation
+    from UQpy.surrogates.gaussian_process.regression_models import LinearRegression
+    from UQpy.surrogates.gaussian_process.kernels.RBF import RBF
 
-    K = Kriging(regression_model=LinearRegression(), correlation_model=ExponentialCorrelation(), optimizations_number=20,
-                correlation_model_parameters=[1, 1], optimizer=MinimizeOptimizer('l-bfgs-b'), )
+    K = GaussianProcessRegression(kernel=RBF(), hyperparameters=[1, 1, 0.1], random_state=0,
+                                  optimizer=MinimizeOptimizer('l-bfgs-b'), regression_model=LinearRegression(),
+                                  optimizations_number=20, bounds=[[1e-2, 20], [1e-2, 20], [1e-5, 1e-2]], )
     model = PythonModel(model_script='python_model_function.py', model_object_name="y_func")
     rmodel = RunModel(model=model)
+    rmodel.run()
     K.fit(samples=x.samples, values=rmodel.qoi_list)
     with pytest.raises(BeartypeCallHintPepParamException):
         refinement = GradientEnhancedRefinement(strata=x.strata_object, runmodel_object='abc',
